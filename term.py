aede65d14da31dfab044fab9edc0a59c2a4b2f92
div72 <60045611+div72@users.noreply.github.com>
2023-01-22 21:23:08 +0300

    term: add module for terminal related utils

---
+++ term.py
@@ -0,0 +1,24 @@
+def black(text: str) -> str:
+    return f"\u001b[30m{text}\u001b[0m"
+
+def red(text: str) -> str:
+    return f"\u001b[31m{text}\u001b[0m"
+
+def green(text: str) -> str:
+    return f"\u001b[32m{text}\u001b[0m"
+
+def yellow(text: str) -> str:
+    return f"\u001b[33m{text}\u001b[0m"
+
+def blue(text: str) -> str:
+    return f"\u001b[34m{text}\u001b[0m"
+
+def magenta(text: str) -> str:
+    return f"\u001b[35m{text}\u001b[0m"
+
+def cyan(text: str) -> str:
+    return f"\u001b[36m{text}\u001b[0m"
+
+def white(text: str) -> str:
+    return f"\u001b[37m{text}\u001b[0m"
+
